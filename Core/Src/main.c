6522553cde1e82e94edc8753a3804107c603c135
lieninghzc <3205489273@qq.com>
2026-07-23 16:05:32 +0800

    0

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
-int main(void)
+int main (void)
 {
     SYSCFG_DL_init();
     delay_init();
     SEGGER_RTT_Init();
 
-    printf("\n=== MSPM0G3507 启动 ===\n");
+    printf("\n=== MSPM0G3507 Startup ===\n");
+    printf("print test\n");
     printf("CPUCLK: %lu Hz\n", (unsigned long)CPUCLK_FREQ);
 
     uint32_t count = 0;
 
-    while (1) {
+    while (1)
+    {
         delay_ms(500);
         DL_GPIO_togglePins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
         printf("[%lu]\n", (unsigned long)++count);
